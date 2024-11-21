c4a34a7aee584688c74e39c6b6230d9f063439be
nitin <nmnitin55@gmail.com>
2024-11-21 06:31:10 -0800

    file

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dash.css'; // Import your custom styles
+import './dash.css';
 import Navbar from './navbar2'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,36 +9,36 @@ import { useEffect } from 'react';
 import tokenchecker from '../api/checktoken';
 import { useNavigate } from 'react-router-dom';
 
-import { postapi2} from '../api/getpost'
+import { postapi2 } from '../api/getpost'
 const FileUpload = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
-const[uploading,setupload]=useState(false);
+  const [uploading, setupload] = useState(false);
   const handleFileChange = (event) => {
     const selectedFile = event.target.files;
     setFile(selectedFile);
-   
-    
-};
-    
 
-useEffect(() => {
-  const checkMyToken = async () => {
-    try {
-      const getToken = await tokenchecker();
-      if (getToken.message === 'Token is valid') {
-        sessionStorage.setItem('id', getToken.data.userId);
-        setLoading(true);
-      } else {
-        navigate('/');
-      }
-    } catch (err) {
-      alert('Error getting token');
-    }
+
   };
-  checkMyToken();
-}, []);
+
+
+  useEffect(() => {
+    const checkMyToken = async () => {
+      try {
+        const getToken = await tokenchecker();
+        if (getToken.message === 'Token is valid') {
+          sessionStorage.setItem('id', getToken.data.userId);
+          setLoading(true);
+        } else {
+          navigate('/');
+        }
+      } catch (err) {
+        alert('Error getting token');
+      }
+    };
+    checkMyToken();
+  }, []);
   const handleRemoveFile = () => {
     setFile(null);
   };
@@ -46,20 +46,20 @@ useEffect(() => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setupload(true)
-if(file==null){
+    if (file == null) {
 
-}else{
-const formData=new FormData();
+    } else {
+      const formData = new FormData();
 
-for (var i=0;i<file.length;i++){
-  formData.append('kerafiles',file[i]);
-}
-   
-const response=await postapi2('http://localhost:7777/fileupload',formData);
-console.log(response);
+      for (var i = 0; i < file.length; i++) {
+        formData.append('kerafiles', file[i]);
+      }
 
-if(response.succcess){setupload(false); toast("file uploaded");}
-}
+      const response = await postapi2('http://localhost:7777/fileupload', formData);
+      console.log(response);
+
+      if (response.succcess) { setupload(false); toast("file uploaded"); }
+    }
   };
   if (!loading) {
     return null;
@@ -67,25 +67,25 @@ if(response.succcess){setupload(false); toast("file uploaded");}
 
   return (
     <>
-    
-    <Navbar/>
-    <ToastContainer />
-    <Container className="d-flex justify-content-center align-items-center min-vh-100">
-      <Row>
-        <Col xs={12} sm={10} md={8} lg={6}>
-          <Card className="p-4 bg-light rounded shadow-sm">
-            <h2 className="text-center mb-4">Upload File</h2>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formFile" className="mb-4">
-                <Form.Label>Choose a file</Form.Label>
-                <Form.Control 
-                  type="file" multiple
-                  onChange={handleFileChange} 
-                  required
-                />
-              </Form.Group>
 
-              {/* {file && (
+      <Navbar />
+      <ToastContainer />
+      <Container className="d-flex justify-content-center align-items-center min-vh-100">
+        <Row>
+          <Col xs={12} sm={10} md={8} lg={6}>
+            <Card className="p-4 bg-light rounded shadow-sm">
+              <h2 className="text-center mb-4">Upload File</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formFile" className="mb-4">
+                  <Form.Label>Choose a file</Form.Label>
+                  <Form.Control
+                    type="file" multiple
+                    onChange={handleFileChange}
+                    required
+                  />
+                </Form.Group>
+
+                {/* {file && (
                 <div className="file-preview mb-4">
                   <Card>
                     <Card.Body>
@@ -103,7 +103,7 @@ if(response.succcess){setupload(false); toast("file uploaded");}
                 </div>
               )} */}
 
-              {/* {
+                {/* {
                 file.map((val)=>{
                   return(
                     <>
@@ -113,16 +113,16 @@ if(response.succcess){setupload(false); toast("file uploaded");}
                 })
               }  */}
 
-              <Button type="submit" variant="primary" className="w-100">
-                Upload
-              </Button>
-              {uploading?<p>Uploading.......</p>:null}
-            </Form>
-          </Card>
-        </Col>
-      </Row>
-      
-    </Container>
+                <Button type="submit" variant="primary" className="w-100">
+                  Upload
+                </Button>
+                {uploading ? <p>Uploading.......</p> : null}
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+
+      </Container>
     </>
   );
 };
